4350695b7c07b33aa33bf05fefe2a31a71678f3f
Solovey Aleksandr <solalex.s.a@gmail.com>
2024-04-09 21:04:54 +0300

    add anim

--- src/modules/Main/MainAbout/MainAbout.ts
+++ src/modules/Main/MainAbout/MainAbout.ts
@@ -518,11 +518,14 @@ function initSequenceAnim() {
     container.querySelector<HTMLCanvasElement>(".main-about__anim");
   const imgList =
     animContainer && animContainer.querySelectorAll<HTMLElement>("img");
+  const infoItems =
+    container &&
+    container.querySelectorAll<HTMLElement>(".main-about-info[data-item]");
 
-  if (container && title && animContainer && imgList) {
+  if (container && title && animContainer && imgList && infoItems) {
     const mm = gsap.matchMedia();
 
-    const breakPoint = 835;
+    const breakPoint = 1;
     mm.add(
       {
         isDesktop: `(min-width: ${breakPoint}px)`,
@@ -533,7 +536,9 @@ function initSequenceAnim() {
           let { isDesktop, isMobile } = context.conditions;
 
           if (isDesktop) {
-            const MAIN_TL = gsap.timeline({
+            const mainTL = gsap.timeline();
+
+            const step_1 = gsap.timeline({
               scrollTrigger: {
                 trigger: container,
                 start: "top bottom",
@@ -543,18 +548,7 @@ function initSequenceAnim() {
               },
             });
 
-            const imgTL = gsap.timeline({
-              scrollTrigger: {
-                trigger: container,
-                start: "bottom bottom",
-                end: `${(imgList.length + 1) * 1}% bottom`,
-                scrub: true,
-                pin: true,
-                markers: true,
-              },
-            });
-
-            MAIN_TL.from(
+            step_1.from(
               title,
               {
                 opacity: 0,
@@ -564,6 +558,30 @@ function initSequenceAnim() {
               },
               "step-1",
             );
+
+            // step-1
+            step_1.from(
+              animContainer,
+              {
+                translateY: "200%",
+                duration: 8,
+                ease: "none",
+              },
+              "step-1",
+            );
+
+            mainTL.add(step_1);
+
+            const imgTL = gsap.timeline({
+              scrollTrigger: {
+                trigger: container,
+                start: "bottom bottom",
+                end: `${(imgList.length + 1) * 1}% bottom`,
+                scrub: true,
+                pin: true,
+                // markers: true,
+              },
+            });
 
             for (let i = 0; i < imgList.length; i++) {
               if (imgList[i - 1]) {
@@ -577,20 +595,66 @@ function initSequenceAnim() {
               });
             }
 
-            // step-1
-            {
-              MAIN_TL.from(
-                animContainer,
-                {
-                  translateY: "200%",
-                  duration: 8,
-                  ease: "none",
-                },
-                "step-1",
-              );
-            }
+            mainTL.add(imgTL, "step-2");
 
-            // MAIN_TL.add(TL);
+            imgTL.to(
+              title,
+              {
+                opacity: 0,
+                translateY: "-100%",
+                duration: 50,
+                ease: "none",
+              },
+              0,
+            );
+
+            if (infoItems.length >= 3) {
+              const sub = gsap.timeline();
+
+              sub.from(infoItems[0], {
+                opacity: 0,
+                translateY: "100%",
+                duration: 50,
+                ease: "none",
+              });
+              sub.to(infoItems[0], {
+                opacity: 0,
+                translateY: "-100%",
+                delay: 30,
+                duration: 50,
+                ease: "none",
+              });
+
+              sub.from(infoItems[1], {
+                opacity: 0,
+                translateY: "100%",
+                duration: 50,
+                ease: "none",
+              });
+              sub.to(infoItems[1], {
+                opacity: 0,
+                translateY: "-100%",
+                delay: 30,
+                duration: 50,
+                ease: "none",
+              });
+
+              sub.from(infoItems[2], {
+                opacity: 0,
+                translateY: "100%",
+                duration: 50,
+                ease: "none",
+              });
+              sub.to(infoItems[2], {
+                opacity: 0,
+                translateY: "-100%",
+                delay: 30,
+                duration: 50,
+                ease: "none",
+              });
+
+              imgTL.add(sub, 20);
+            }
 
             return;
           } else if (isMobile) {
